f799a08affd1649b6b3a58833b343c66fb939f2c
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2026-04-04 11:51:10 -0400

    Update: word-ladder - submission-1

--- Data Structures & Algorithms/word-ladder/submission-1.py
+++ Data Structures & Algorithms/word-ladder/submission-1.py
@@ -1,8 +1,9 @@
 from collections import deque, defaultdict
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord == beginWord or endWord not in wordList:
+        if beginWord == endWord or endWord not in wordList:
             return 0
+        wordList.append(beginWord)
         word_map = defaultdict(list)
         for word in wordList:
             for ind in range(len(word)):
@@ -12,15 +13,15 @@ class Solution:
         q = deque([beginWord])
         res = 1
         while q:
-            for ind in range(len(q)):
+            for each in range(len(q)):
                 word = q.popleft()
                 if word == endWord:
                     return res
                 for ind in range(len(word)):
                     pattern = word[:ind] + '*' + word[ind+1:]
-                    for each in word_map[pattern]:
-                        if each not in seen:
-                            seen.add(each)
-                            q.append(each)
+                    for neighbor in word_map[pattern]:
+                        if neighbor not in seen:
+                            seen.add(neighbor)
+                            q.append(neighbor)
             res += 1
         return 0
