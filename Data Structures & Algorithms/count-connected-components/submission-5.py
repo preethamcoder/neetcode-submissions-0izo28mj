7bfb93888f7bb02685980958261862b6ea459a10
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2026-04-04 00:31:00 -0400

    Add: count-connected-components - submission-5

---
+++ Data Structures & Algorithms/count-connected-components/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        parents = [ind for ind in range(n)]
+        ranks = [1]*n
+        res = n
+        def find(node):
+            res = node
+            while res != parents[res]:
+                parents[res] = parents[parents[res]]
+                res = parents[res]
+            return res
+        def union(n1, n2):
+            p1 = find(n1)
+            p2 = find(n2)
+            if p1 == p2:
+                return 0
+            if ranks[p1] >= ranks[p2]:
+                parents[p2] = p1
+                ranks[p1] += 1
+            else:
+                parents[p1] = p2
+                ranks[p2] += 1
+            return 1
+        for s, v in edges:
+            res -= union(s, v)
+        return res
