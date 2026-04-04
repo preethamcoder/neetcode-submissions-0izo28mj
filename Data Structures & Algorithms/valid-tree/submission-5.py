8da91247aa88ba6c8d5e3c5568ff431ac3f8791c
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2026-04-04 00:06:54 -0400

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # if not edges:
+        #     return True
+        course_map = {ind:[] for ind in range(n)}
+        for s, v in edges:
+            course_map[s].append(v)
+            course_map[v].append(s)
+        seen = set()
+        def dfs(node, parent):
+            if node in seen:
+                return False
+            seen.add(node)
+            for each in course_map[node]:
+                if each == parent:
+                    continue
+                if not dfs(each, node):
+                    return False
+            return True
+        return dfs(0, -1) and len(seen) == n
