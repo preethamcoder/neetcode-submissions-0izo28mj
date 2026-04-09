829348c0ff52977dc83ff394d2fb0ec41b7e7444
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2026-04-08 21:32:18 -0400

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        tickets.sort()
+        length = len(tickets)
+        seen = set()
+        res = ['JFK']
+        graph = defaultdict(list)
+        for s, v in tickets:
+            graph[s].append(v)
+        def dfs(src):
+            if len(res) == length + 1:
+                return res
+            if src not in graph:
+                return False
+            seen.add(src)
+            tmp = graph[src]
+            for ind, ver in enumerate(tmp):
+                res.append(ver)
+                graph[src].remove(ver)
+                if dfs(ver):
+                    return True
+                res.pop()
+                graph[src].insert(ind, ver)
+            return False
+        dfs('JFK')
+        return res
